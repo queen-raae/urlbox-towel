a8731dc72eebee696094a52a8dd30e18f9bef106
Benedicte Raae <raae@raae.codes>
2023-11-18 12:59:42 +0100

    feat: add email form (#14)
    * add email form
    
    * use env variable for outseta url

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly PUBLIC_OUTSETA_EMAIL_LIST_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
