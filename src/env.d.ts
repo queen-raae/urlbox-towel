c8ed873dec2added28e6721ba5a2967f09099195
raae <queen@raae.codes>
2024-02-17 10:35:41 +0100

    chore: tinystack

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,6 +2,11 @@
 
 interface ImportMetaEnv {
   readonly PUBLIC_OUTSETA_EMAIL_LIST_URL: string;
+  // Tiny Stack
+  readonly REPLICA_URL: string;
+  readonly REPLICA_BUCKET: string;
+  readonly LITESTREAM_ACCESS_KEY_ID: string;
+  readonly LITESTREAM_SECRET_ACCESS_KEY: string;
 }
 
 interface ImportMeta {
