0355a9a602c1b382eb57ef2e984f4117e0cd44e8
Ola <ola@olavea.com>
2024-06-06 13:49:45 +0200

    rough changes to make it beachy

--- db/config.ts
+++ db/config.ts
@@ -1,42 +1,13 @@
 import { defineDb, defineTable, column } from "astro:db";
 
-const Comment = defineTable({
+const Screenshot = defineTable({
   columns: {
     id: column.number({ primaryKey: true }),
-    author: column.text({ optional: true }),
-    content: column.text({ optional: true }),
-  },
-});
-
-const Playlist = defineTable({
-  columns: {
-    id: column.number({ primaryKey: true }),
-    name: column.text(),
-    description: column.text({ optional: true }),
-    createdBy: column.text({ optional: true }),
-  },
-});
-
-const Episode = defineTable({
-  columns: {
-    id: column.text({ primaryKey: true }),
-    title: column.text(),
-    description: column.text({ optional: true }),
-    audio: column.text(),
-  },
-});
-
-const PlaylistEpisode = defineTable({
-  columns: {
-    playlistId: column.number({
-      references: () => Playlist.columns.id,
-    }),
-    episodeId: column.text({
-      references: () => Episode.columns.id,
-    }),
+    tweetUrl: column.text({ optional: false }),
+    renderUrl: column.text({ optional: true }),
   },
 });
 
 export default defineDb({
-  tables: { Comment, Playlist, Episode, PlaylistEpisode },
+  tables: { Screenshot },
 });
